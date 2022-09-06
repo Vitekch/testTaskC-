2e909d5606b493789d0f6490fc3a817f29039bba
vchizhikov <vchizhikov@zuzex.com>
2022-09-06 13:39:38 +0300

    Добавьте файлы проекта.

---
+++ FigureSquare/FigureSquare.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace FigureSquare
+{
+
+    public class Square
+    { 
+        static public double FindSquare(double R)
+        {
+            if (R < 0)
+                throw new Exception("Incorrect values");
+            return Math.PI * Math.Pow(R, 2);
+        }
+
+        static public double FindSquare(double A, double B, double C)
+        {
+            if (A < 0 || B < 0 || C < 0)
+                throw new Exception("Incorrect values");
+            bool isRT = C == Math.Sqrt(Math.Pow(A, 2) + Math.Pow(B, 2)) ? true : false;
+            if (isRT)
+            {
+                return A * B / 2;
+            }
+            double p = (A + B + C) / 2;
+            return Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+        }
+    }
+}
---
+++ TestLibrary/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using FigureSquare;
+
+namespace TestLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double circleSquare = Square.FindSquare(2.3);
+            double rTriangleSquare = Square.FindSquare(3, 4, 5);
+            double triangleSquare = Square.FindSquare(6, 3, 4);
+
+            Console.WriteLine(circleSquare);
+            Console.WriteLine(rTriangleSquare);
+            Console.WriteLine(triangleSquare);
+
+            Console.Read();
+        }
+    }
+}
